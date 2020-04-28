4922149d619e10bb9fbbcc10f24c3746c89535bc
aordorica <alaniordorica@gmail.com>
2020-04-28 13:42:22 -0700

    easyPieChart skills section added

--- js/script.js
+++ js/script.js
@@ -15,3 +15,37 @@ var options = {
 };
 
 var typed = new Typed('.typed', options);
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    items: 4,
+    responsive: {
+        0: {
+            items: 1
+        },
+        480: {
+            items: 2
+        },
+        768: {
+            items: 3
+        },
+        938:{
+            items: 4
+        }
+    }
+})
+
+$(function () {
+    $('.chart').easyPieChart({
+        //your options goes here
+        easing: 'easeInOut',
+        barColor: '#fff',
+        trackColor: false,
+        scaleColor: '#000',
+        lineWidth: 4,
+        size: 152,
+        onStep: function(from, to, percent) {
+            $(this.el).find('.percent').text(Math.round(percent));
+        }
+    });
+});
