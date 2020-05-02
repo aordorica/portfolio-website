e1fc7678ed44b14db56a4b2b2082c156a868a191
Alan Ordorica <alaniordorica@gmail.com>
2020-05-02 15:17:33 -0700

    skills added

--- js/script.js
+++ js/script.js
@@ -1,51 +1,58 @@
 $(document).ready(function () {
     $('#slides').superslides({
         animation: 'fade',
-        play: 8000,
+        play: 5000,
         pagination: false
     });
-});
 
-var options = {
-    strings: ['Computer Engineer.', 'Web Developer.', 'Student'],
-    typeSpeed: 70,
-    showCursor: false,
-    loop: true,
-    startDelay: 1000
-};
+    var options = {
+        strings: ['Computer Engineer.', 'Web Developer.', 'Student'],
+        typeSpeed: 70,
+        showCursor: false,
+        loop: true,
+        startDelay: 1000
+    };
 
-var typed = new Typed('.typed', options);
+    var typed = new Typed('.typed', options);
 
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    items: 4,
-    responsive: {
-        0: {
-            items: 1
-        },
-        480: {
-            items: 2
-        },
-        768: {
-            items: 3
-        },
-        938:{
-            items: 4
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        items: 4,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            938: {
+                items: 4
+            }
         }
-    }
-})
+    })
 
-$(function () {
-    $('.chart').easyPieChart({
-        //your options goes here
-        easing: 'easeInOut',
-        barColor: '#fff',
-        trackColor: false,
-        scaleColor: '#000',
-        lineWidth: 4,
-        size: 152,
-        onStep: function(from, to, percent) {
-            $(this.el).find('.percent').text(Math.round(percent));
+    var skillsTopOffset = $(".skillsSection").offset().top;
+
+    $(window).scroll(function () {
+        if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+
+            $(function () {
+                $('.chart').easyPieChart({
+                    //your options goes here
+                    easing: 'easeInOut',
+                    barColor: '#fff',
+                    trackColor: false,
+                    scaleColor: '#000',
+                    lineWidth: 4,
+                    size: 152,
+                    onStep: function (from, to, percent) {
+                        $(this.el).find('.percent').text(Math.round(percent));
+                    }
+                });
+            });
         }
-    });
+    })
 });
