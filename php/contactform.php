b99d4d5a7d2e02fe329522ae043e8d0832dc9070
aordorica <alaniordorica@gmail.com>
2020-05-22 17:01:14 -0700

    Added php email form

--- php/contactform.php
+++ php/contactform.php
@@ -7,10 +7,19 @@ if (isset($_POST['submit'])) {
     $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
 
-    $mailTo = "alaniordorica@gmail.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "You Have received an e-mail from ".$name."..\n\n".$message;
+    $mailTo = "alan@alanordorica.com";
+    $headers .= "Reply-To: .$mailFrom\r\n"; 
+    $headers .= "Return-Path: .$mailFrom\r\n"; 
+    $headers .= "From: .$mailFrom\r\n";  
+    $headers .= "Organization: Sender Organization\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+    $headers .= "X-Priority: 3\r\n";
+    $headers .= "X-Mailer: PHP". phpversion() ."\r\n" ;
+
+
+    $txt = "New Website contact! You Have received an e-mail from: ".$name.".\n\n".$message;
 
     mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.php?mailsend");
+    header("Location: ../index.html?mailsend");
 }
