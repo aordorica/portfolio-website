4560152de34f98785c7805d0a33818cd932a2019
Alan Ordorica <alaniordorica@gmail.com>
2020-07-31 01:36:55 -0700

    Included navbar transition with JS/JQuery

--- js/script.js
+++ js/script.js
@@ -1,105 +1,129 @@
+var options = {
+  strings: ["View", "Resume"],
+  typeSpeed: 70,
+  showCursor: false,
+  loop: true,
+  startDelay: 1000,
+};
 
-  var options = {
-    strings: ["View", "Resume"],
-    typeSpeed: 70,
-    showCursor: false,
-    loop: true,
-    startDelay: 1000
-  };
+// ***********************************************************************************************
+//                        Typed Effect using Typed.js file
+// ***********************************************************************************************
+var typed = new Typed(".typed", {
+  strings: ["Hello World, I'm"],
+  typeSpeed: 80,
+  loop: true,
+  startDelay: 1000,
+  showCursor: false,
+});
 
-  var typed = new Typed(".typed", options);
-
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    items: 4,
-    responsive: {
-      0: {
-        items: 1
-      },
-      480: {
-        items: 2
-      },
-      768: {
-        items: 3
-      },
-      800: {
-        items: 4
-      }
-    }
-  });
-
-  var skillsTopOffset = $(".skillsSection").offset().top;
-
-  $(window).scroll(function() {
-    if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
-      $(function() {
-        $(".chart").easyPieChart({
-          //your options goes here
-          easing: "easeInOut",
-          barColor: "#7AB8A4",
-          trackColor: false,
-          scaleColor: "#000",
-          lineWidth: 4,
-          size: 152,
-          onStep: function(from, to, percent) {
-            $(this.el)
-              .find(".percent")
-              .text(Math.round(percent));
-          }
-        });
+// ***********************************************************************************************
+//                        Navigation Bar Scrolling Effect
+// ***********************************************************************************************
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(window).scrollTop() < $(window).height() - 90) {
+      $("#navigation").css({ "background-color": "transparent" });
+      $("#navigation").css({
+        "box-shadow": "none",
       });
+    } else {
+      $("#navigation").css({ "background-color": "#16171a" });
+      $("#navigation").css({ "box-shadow": "rgba(2, 12, 27, 0.7) 0px 10px 30px -10px" });
     }
   });
+});
 
-  $("[data-fancybox]").fancybox();
+// Stats Carousel
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  items: 4,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    480: {
+      items: 2,
+    },
+    768: {
+      items: 3,
+    },
+    800: {
+      items: 4,
+    },
+  },
+});
 
-  // Isotope Styling
+var skillsTopOffset = $(".skillsSection").offset().top;
+
+$(window).scroll(function () {
+  if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+    $(function () {
+      $(".chart").easyPieChart({
+        //your options goes here
+        easing: "easeInOut",
+        barColor: "#7AB8A4",
+        trackColor: false,
+        scaleColor: "#000",
+        lineWidth: 4,
+        size: 152,
+        onStep: function (from, to, percent) {
+          $(this.el).find(".percent").text(Math.round(percent));
+        },
+      });
+    });
+  }
+});
+
+$("[data-fancybox]").fancybox();
+
+// Isotope Styling
+$(".items").isotope({
+  filter: "*",
+  animationOptions: {
+    duration: 1500,
+    easing: "linear",
+    queue: false,
+  },
+});
+
+$("#filters a").click(function () {
+  $("#filters .current").removeClass("current");
+  $(this).addClass("current");
+
+  var selector = $(this).attr("data-filter");
   $(".items").isotope({
-    filter: "*",
+    filter: selector,
     animationOptions: {
       duration: 1500,
       easing: "linear",
-      queue: false
-    }
+      queue: false,
+    },
   });
 
-  $("#filters a").click(function() {
-    $("#filters .current").removeClass("current");
-    $(this).addClass("current");
+  return false;
+});
 
-    var selector = $(this).attr("data-filter");
-    $(".items").isotope({
-      filter: selector,
-      animationOptions: {
-        duration: 1500,
-        easing: "linear",
-        queue: false
-      }
-    });
+$("#navigation a").click(function (e) {
+  e.preventDefault();
+  var targetElement = $(this).attr("href");
+  var targetPosition = $(targetElement).offset().top;
+  $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
+});
 
-    return false;
-  });
+const nav = $("#navigation");
+const navTop = nav.offset().top;
 
-  $("#navigation a").click(function(e) {
-    e.preventDefault();
-    var targetElement = $(this).attr("href");
-    var targetPosition = $(targetElement).offset().top;
-    $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
-  });
+$(window).on("scroll", stickyNavigation);
 
-  const nav = $("#navigation");
-  const navTop = nav.offset().top;
+function stickyNavigation() {
+  var body = $("body");
 
-  $(window).on("scroll", stickyNavigation);
-
-  function stickyNavigation() {
-    var body = $("body");
-
-    if ($(window).scrollTop() >= navTop) {
-      body.css("padding-top", nav.outerHeight() + "px");
-      body.addClass("fixedNav");
-    } else {
-      body.css("padding-top", 0);
-      body.removeClass("fixedNav");
-    }
-  };
+  if ($(window).scrollTop() >= navTop) {
+    body.css("padding-top", nav.outerHeight() + "px");
+    body.addClass("fixedNav");
+  } else {
+    body.css("padding-top", 0);
+    body.removeClass("fixedNav");
+  }
+}
