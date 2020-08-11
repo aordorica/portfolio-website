647f6dcfa8c34717fbb7d85ba19820d4f80d2ee8
Alan Ordorica <alaniordorica@gmail.com>
2020-08-11 15:37:02 -0700

    Styling contact, and about sections complete

--- js/script.js
+++ js/script.js
@@ -29,9 +29,69 @@ $(document).ready(function () {
       });
     } else {
       $("#navigation").css({ "background-color": "#272c35" });
-      $("#navigation").css({ "box-shadow": "rgba(2, 12, 27, 0.7) 0px 10px 30px -10px" });
+      $("#navigation").css({
+        "box-shadow": "rgba(2, 12, 27, 0.7) 0px 10px 30px -10px",
+      });
     }
   });
+});
+
+// ***********************************************************************************************
+//                        HighLight Current Section in Navigation Bar
+// ***********************************************************************************************
+
+// Cache selectors
+var lastId,
+  topMenu = $("#mainNav"),
+  topMenuHeight = topMenu.outerHeight() + 1,
+  // All list items
+  menuItems = topMenu.find("a"),
+  // Anchors corresponding to menu items
+  scrollItems = menuItems.map(function () {
+    var item = $($(this).attr("href"));
+    if (item.length) {
+      return item;
+    }
+  });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function (e) {
+  var href = $(this).attr("href"),
+    offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+  $("html, body").stop().animate(
+    {
+      scrollTop: offsetTop,
+    },
+    850
+  );
+  e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function () {
+  // Get container scroll position
+  var fromTop = $(this).scrollTop() + topMenuHeight;
+
+  // Get id of current scroll item
+  var cur = scrollItems.map(function () {
+    if ($(this).offset().top < fromTop) return this;
+  });
+  // Get the id of the current element
+  cur = cur[cur.length - 1];
+  var id = cur && cur.length ? cur[0].id : "";
+
+  if (lastId !== id) {
+    lastId = id;
+    // Set/remove active class
+    menuItems
+      .parent()
+      .removeClass("active")
+      .end()
+      .filter("[href=#" + id + "]")
+      .parent()
+      .addClass("active");
+  }
 });
 
 // ***********************************************************************************************
@@ -106,4 +166,3 @@ $("#filters a").click(function () {
 
   return false;
 });
-
