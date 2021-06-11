801d259272a50b95c1091b4fb068b08a61a6e39c
Alan Ordorica <alaniordorica@gmail.com>
2021-06-11 16:42:17 -0700

    file audit to resolve dependancy issue

--- Javascript/script.js
+++ Javascript/script.js
@@ -8,28 +8,6 @@ var options = {
 
 var typed = new Typed("#typed", options); 
 
-(function ($) {
-  "use strict";
-
-  var $navbar = $("#navbar"),
-    y_pos = $navbar.offset().top,
-    height = $navbar.height();
-
-  $(document).scroll(function () {
-    var scrollTop = $(this).scrollTop();
-
-    if (scrollTop > y_pos + height) {
-      $navbar.addClass("navbar-fixed").animate({
-        top: 0,
-      });
-    } else if (scrollTop <= y_pos) {
-      $navbar.removeClass("navbar-fixed").clearQueue().animate(
-        {
-          top: "-48px",
-        },
-        0
-      );
-    }
-  });
-})(jQuery, undefined);
-
+window.addEventListener("scroll", function () {
+  console.log("scrolling....")
+});
