e6659bb72f723224ece9b81d459d82272e23bd76
Alan Ordorica <alaniordorica@gmail.com>
2021-07-09 12:22:40 -0700

    re-created using react. Initial setup using react-router implementation and webpack

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import Header from "../components/Header";
+import NotFoundPage from "../components/NotFoundPage";
+
+
+const AppRouter = () => (
+    <BrowserRouter>
+        <div>
+            <Header />
+            <Switch>
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>
+    </BrowserRouter>
+);
+
+export default AppRouter;
