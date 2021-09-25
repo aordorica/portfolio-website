0c43d7062c65155318204954d5a10b6185507385
Alan Ordorica <alaniordorica@gmail.com>
2021-09-24 20:49:15 -0700

    fixed Routers directory

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from './Routers/AppRouter';
+import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import "bootstrap/dist/css/bootstrap.min.css";
