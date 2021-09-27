e1493d19d6e5e95198bc5bb8666137809647d38f
Alan Ordorica <alaniordorica@gmail.com>
2021-09-26 17:24:03 -0700

    public domain added to requests url

--- frontend/src/routers/AppRouter.js
+++ frontend/src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NotFoundPage from '../Components/NotFoundPage';
 import HomePage from '../Components/HomePage';
 import ContactPage from '../Components/ContactPage';
