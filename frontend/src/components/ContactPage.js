1d5bc8d6dc8ed9dc4faa6da7de4789874c477f62
Alan Ordorica <alaniordorica@gmail.com>
2021-09-28 00:13:08 -0700

    changed rewuest to secure https

--- frontend/src/components/ContactPage.js
+++ frontend/src/components/ContactPage.js
@@ -10,7 +10,7 @@ import validator from 'validator'
 import { ReactComponent as ContactIcon} from '../../public/images/contact.svg'
 import axios from 'axios'
 
-const servURL = "http://www.alanordorica.com/contact";
+const servURL = "https://www.alanordorica.com/contact";
 
 const ContactPage = () => {
     const [formData, setFormData] = useState({
