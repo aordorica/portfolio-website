633960388c37081a717fceed85c35711360512d9
Alan Ordorica <alaniordorica@gmail.com>
2021-09-29 00:48:03 +0000

    enabled cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-// app.use(cors())
+app.use(cors())
 
 // const pubURL = "https://www.alanordorica.com";
 
