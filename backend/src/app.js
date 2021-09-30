4929140739360ac8d3d7498fa53f99a8f964638c
Alan Ordorica <alaniordorica@gmail.com>
2021-09-30 00:28:39 -0700

    added navbar modal and resume download options

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,53 +1,31 @@
-const express = require('express');
-const sendContactEmail = require('./emails/send')
+const express = require("express");
+const sendContactEmail = require("./emails/send");
 const app = express();
-const cors = require('cors')
-const port = process.env.PORT || 3000
+const cors = require("cors");
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
-// app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-// const pubURL = "https://www.alanordorica.com";
-
-// app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", pubURL);
-//     res.setHeader("Access-Control-Allow-Methods", 'POST');
-//     res.setHeader("Access-Control-Allow-Headers", 'Content-Type');
-//     return next();
-// })
-
-// const corsCheck = (req, res, next) => {
-//     if(req.header.origin === pubURL) {
-//         console.log('Origin secured!')
-//         return next()
-//     } else {
-//         console.log('Invalid origin!')
-//         res.status(401).send({
-//             error: 'Invalid origin!'
-//         })
-//     }
-// }
-
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
     res.send({
-        title: "GET email",
+        title: "GET /api",
         name: "Alan Ordorica",
     });
 });
 
-
-app.post('/', (req, res) => {
-    console.log('Gotten into the POST section');
-    const emailData = { ...req.body }
+app.post("/api", (req, res) => {
+    console.log("Gotten into the POST section");
+    const emailData = { ...req.body };
     try {
         sendContactEmail({ ...req.body });
         console.log(emailData);
         res.status(200).send({ message: "Message Sent! Thank you!" });
     } catch (error) {
-        res.status(400).send(error)
+        res.status(400).send(error);
     }
-})
+});
 
 app.listen(port, () => {
     console.log(`Node App is running on port: ${port}`);
-})
+});
